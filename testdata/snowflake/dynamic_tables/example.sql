8a005a52426570083a373a4ce6382f7c2970d3f0
Graham <GrahamTheCoder@gmail.com>
2025-09-09 13:15:16 +0100

    workaround

--- testdata/snowflake/dynamic_tables/example.sql
+++ testdata/snowflake/dynamic_tables/example.sql
@@ -1,8 +1,8 @@
 create or replace transient dynamic table my_schema.example_table (
     something integer not null
 )
-target_lag = 'DOWNSTREAM' refresh_mode = incremental initialize = on_create warehouse = test_warehouse
 cluster by (trunc(something, -2))
+target_lag = 'DOWNSTREAM' refresh_mode = incremental initialize = on_create warehouse = test_warehouse
 as
 (
     select 1 as something
